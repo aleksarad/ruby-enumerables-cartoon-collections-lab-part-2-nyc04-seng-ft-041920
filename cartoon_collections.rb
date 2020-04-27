24f8939858fa9601f20aa8005fde3109c82699ca
Alexandra Radevich <alexa.radevich1@gmail.com>
2020-04-27 20:25:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,8 @@ def summon_captain_planet(planeteer_calls)
   # Return a new array of the results
   
   planeteer_calls.map do |el|
-end
+    "#{el.capitalize()}!"
+  end 
 end
 
 def long_planeteer_calls(planeteer_calls)
